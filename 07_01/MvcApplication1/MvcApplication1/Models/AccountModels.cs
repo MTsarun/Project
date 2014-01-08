ce110732225029e3b93e0c2fbb4bcf10767c7891
MTsarun <marya.tsarun@gmail.com>
2014-01-08 21:17:01 +0300

    Try_2. Nothing new from last version.

---
+++ 07_01/MvcApplication1/MvcApplication1/Models/AccountModels.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Globalization;
+using System.Web.Security;
+
+namespace MvcApplication1.Models
+{
+    public class UsersContext : DbContext
+    {
+        public UsersContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+
+    [Table("Users")]
+    public class User
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public bool Books { get; set; }
+        public bool Movies { get; set; }
+        public bool Travelling { get; set; }
+    }
+
+    public class RegisterExternalLoginModel
+    {
+        [Required]
+        [Display(Name = "Имя пользователя")]
+        public string UserName { get; set; }
+
+        public string ExternalLoginData { get; set; }
+    }
+
+    public class LocalPasswordModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Текущий пароль")]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Значение \"{0}\" должно содержать не менее {2} символов.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Новый пароль")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Подтверждение пароля")]
+        [Compare("NewPassword", ErrorMessage = "Новый пароль и его подтверждение не совпадают.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class LocalProfileModel
+    {
+        [Required]
+        [DataType(DataType.Name)]
+        [Display(Name = "Actual name")]
+        public string ActualName { get; set; }
+
+        [Required]
+        [DataType(DataType.Name)]
+        [Display(Name = "New name")]
+        public string NewName { get; set; }
+
+        
+    }
+
+    public class LoginModel
+    {
+        [Required]
+        [Display(Name = "Your email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterModel
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Your email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Значение \"{0}\" должно содержать не менее {2} символов.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Repeat password")]
+        [Compare("Password", ErrorMessage = "Passwords are not equal.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Age")]
+        public int Age { get; set; }
+
+        [Required]
+        [Display(Name = "Books")]
+        public bool Books { get; set; }
+
+        [Required]
+        [Display(Name = "Movies")]
+        public bool Movies { get; set; }
+
+        [Required]
+        [Display(Name = "Travelling")]
+        public bool Travelling { get; set; }
+    }
+
+
+    public class ExternalLogin
+    {
+        public string Provider { get; set; }
+        public string ProviderDisplayName { get; set; }
+        public string ProviderUserId { get; set; }
+    }
+}
